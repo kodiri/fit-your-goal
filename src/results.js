f4d1df35c83e99d6e905a5abedd01712024186e8
Tom Hodson <tomhodson91@hotmail.com>
2018-12-05 12:04:47 +0000

    added 3 main pages

---
+++ src/results.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+export default function results() {
+    return (
+        <div>
+            <Link to= {"/"}>
+                <div className='links'>
+                    Home
+                </div>
+            </Link>
+            <Link to= {"/get-started"}>
+                <div className='links'>
+                    Back to get started!
+                </div>
+            </Link>
+        </div>
+    );
+}
