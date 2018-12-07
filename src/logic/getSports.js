f628ceaa9c9f97072b7b61f38baf18d9aa593166
Tom Hodson <tomhodson91@hotmail.com>
2018-12-07 14:28:01 +0000

    functions for filter logic

--- src/logic/getSports.js
+++ src/logic/getSports.js
@@ -1,23 +1,36 @@
 function getSports(age, weight, goals) {
     let sports = ['run', 'bike', 'swim', 'row', 'walk', 'yoga', 'weights']
-    if (age > 60) {
-        sports = sports.filter(function (sport) {
+    sports = filterByAge(sports, age);
+    sports = filterByWeight(sports, weight);
+    sports = filterByGoals(sports, goals);
+    return sports;
+}
+
+function filterByAge(sports, age) {
+    return age > 60 ?
+        sports.filter(function (sport) {
             return sport !== 'weights' &&
                 sport !== 'run' &&
                 sport !== 'row';
-        })
-    }
-    if (weight > 100) {
-        sports = sports.filter(function (sport) {
-            return sport !== 'run';
-        })
-    }
-    if (goals === 'Lose Weight') {
-        sports = sports.filter(function (sport) {
-            return sport !== 'weights';
-        })
-    }
-    return sports;
+        }) :
+        sports;
 }
+
+function filterByWeight(sports, weight) {
+    return weight > 100 ?
+        sports.filter(function (sport) {
+            return sport !== 'run';
+        }) :
+        sports;
+}
+
+function filterByGoals(sports, goals) {
+    return goals === 'Lose Weight' ?
+        sports.filter(function (sport) {
+            return sport !== 'weights';
+        }) :
+        sports;
+}
+
 //example calling function for user with: age 65, weight 101kg, goal 'Lose Weight'
 getSports(65, 101, 'Lose Weight') //expected output: ["bike", "swim", "walk", "yoga"]
