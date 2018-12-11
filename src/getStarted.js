85fb9368f8170cca6410a35e51e15fe672d1a07e
maruntav <osman_990@hotmail.com>
2018-12-11 14:46:19 +0000

    made changes to the getStarted file (#8)
    * made changes to the getStarted file
    
    * package lock file updated
    
    * removed the bind for the events in the getStarted file and used arrow functions

--- src/getStarted.js
+++ src/getStarted.js
@@ -1,19 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
 
-export default function getStarted() {
-    return (
-        <div>
-            <Link to= {'/'}>
-                <div className='links'>
-                    Home
-                </div>
-            </Link>
-            <Link to= {'/results'}>
-                <div className='links'>
-                    Submit!
-                </div>
-            </Link>
-        </div>
-    );
+export default class GetStarted extends Component{
+    constructor(){
+        super();
+        this.state = {
+            fields: {
+                age: ''
+            },
+            error_msg: {
+                age: ''
+            }
+        }
+    }
+
+    handleChange(e){
+        this.setState({
+            fields: {
+                [e.target.name]: e.target.value
+            },
+            error_msg: {
+                age: ''
+            }
+        });
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        if(!this.state.fields.age){
+            this.setState({
+                error_msg: {
+                    age: 'Field is empty'
+                }
+            });
+        }
+        else{
+            this.props.history.push('/results', this.state.fields);
+        }
+    }
+
+    render(){
+        return(
+            <div className='get-started'>
+                <form className='get-started-form' onSubmit={(e) => this.handleSubmit(e)}>
+                    <label>
+                        Age
+                        <input type='text' name='age' value={this.state.fields.age} onChange={(e) => this.handleChange(e)} />
+                    </label>
+                    <div style={{display: 'inline-block', color: 'red'}}>{this.state.error_msg.age}</div>
+                    <br/>
+                    <input type='submit' value='Get Started'/>
+                </form>
+            </div>
+        );
+    }
 }
