9853f7293abb0acd8e3a33e45c509eda563ad3c0
Osman Hussein <osman_990@hotmail.com>
2018-12-16 20:45:17 +0000

    the form is working again but tests is still incomplete

--- src/get-started/get-started-utils/GetStartedUtils.js
+++ src/get-started/get-started-utils/GetStartedUtils.js
@@ -1,15 +1,58 @@
-export default class Utils{
-    displayGoalDefaultOrNot(optionVal){
-        //This makes sure that the user doesn't ignore the goals option
-        if(optionVal === 'instruction'){
-            return 'block';
-        }
-        return 'none';
-    }
 
-    kebabCaseToWords(words){
-        return words.split('-')
-            .map((word) => word[0].toUpperCase() +word.substring(1))
-            .join(' ');
+function displayGoalDefaultOrNot(optionVal){
+    //This makes sure that the user doesn't ignore the goals option
+    if(optionVal === 'instruction'){
+        return 'block';
     }
+    return 'none';
 }
+
+function kebabCaseToWords(words){
+    return words.split('-')
+        .map((word) => word[0].toUpperCase() +word.substring(1))
+        .join(' ');
+}
+
+function updateFields(prevVals, target){
+    return Object.keys(prevVals).reduce((newValues, field) => {
+        if(field === target.name){
+            newValues[field] = target.value; 
+        }
+        else{
+            newValues[field] = prevVals[field];
+        }
+        return newValues;
+    }, {});
+}
+
+function validateGoalsOptions(curr_err, goals_val){
+    let isDefault = goals_val === 'instruction';
+    curr_err.goal = (isDefault) ? 'Please select an option' : '';
+    return curr_err;
+}
+
+function everythingFilled(fieldsObj){
+    let newErrorMessages = Object.keys(fieldsObj).reduce((newObj, field) => {
+        let empty = !fieldsObj[field];
+        newObj[field] = (empty) ? `Please fill in the ${field}` : '';
+        return newObj;
+    }, {});
+    return newErrorMessages;
+}
+
+function createEmptyFieldObj(valueArray){
+    return valueArray
+    .reduce((emptyValues, name) => {
+        emptyValues[name] = '';
+        return emptyValues;
+    }, {});
+}
+
+//If there are no error messages then it returns true
+function checkIfEverythingIsGood(error_messages){
+    return  Object.keys(error_messages).reduce((allGood, name) => {
+        return allGood && error_messages[name] === '';
+    }, true);
+}
+
+export {createEmptyFieldObj, everythingFilled, validateGoalsOptions, displayGoalDefaultOrNot, kebabCaseToWords, updateFields, checkIfEverythingIsGood};
--- src/getStarted.js
+++ src/getStarted.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as startUtils from './get-started/get-started-utils/GetStartedUtils';
 
 export default class GetStarted extends Component{
     constructor(){
@@ -25,22 +26,10 @@ export default class GetStarted extends Component{
     }
 
     handleChange(e){
-        let newValues = this.updateFields(this.state.fields, e.target);
+        let newValues = startUtils.updateFields(this.state.fields, e.target);
         this.setState({
             fields: newValues
         });
-    }
-
-    updateFields(prevVals, target){
-        return Object.keys(prevVals).reduce((newValues, field) => {
-            if(field === target.name){
-                newValues[field] = target.value; 
-            }
-            else{
-                newValues[field] = prevVals[field];
-            }
-            return newValues;
-        }, {});
     }
 
     clearErrorMessages(){
@@ -49,13 +38,7 @@ export default class GetStarted extends Component{
         });
     }
 
-    createEmptyFieldObj(){
-        return this.allnames
-        .reduce((emptyValues, name) => {
-            emptyValues[name] = '';
-            return emptyValues;
-        }, {});
-    }
+    
 
     handleSubmit(e){
         e.preventDefault();
@@ -65,27 +48,10 @@ export default class GetStarted extends Component{
         }
     }
 
-    validateGoalsOptions(curr_err, goals_val){
-        let isDefault = goals_val === 'instruction';
-        curr_err.goal = (isDefault) ? 'Please select an option' : '';
-        return curr_err;
-    }
-
-    everythingFilled(fieldsObj){
-        let filled = true;
-        let newErrorMessages = Object.keys(fieldsObj).reduce((newObj, field) => {
-            let empty = !fieldsObj[field];
-            newObj[field] = (empty) ? `Please fill in the ${field}` : '';
-            if(!empty && filled) filled = false;
-            return newObj;
-        }, {});
-        return newErrorMessages;
-    }
-
     validateAllEverything(){
         let numsErrorMessages = this.validateNumericalFields();
-        let numsWithGoalsErr = this.validateGoalsOptions(numsErrorMessages, this.state.goalOptionVal);
-        if(this.checkIfEverythingIsGood(numsWithGoalsErr)){
+        let numsWithGoalsErr = startUtils.validateGoalsOptions(numsErrorMessages, this.state.goalOptionVal);
+        if(startUtils.checkIfEverythingIsGood(numsWithGoalsErr)){
             return true;
         }
         this.setState({
@@ -96,45 +62,11 @@ export default class GetStarted extends Component{
 
     
     validateNumericalFields(){
-        let checkIfEmpty = this.everythingFilled(this.state.fields);
+        let checkIfEmpty = startUtils.everythingFilled(this.state.fields);
         return checkIfEmpty;
     }
 
-    //If there are no error messages then it returns true
-    checkIfEverythingIsGood(error_messages){
-        return  Object.keys(error_messages).reduce((allGood, name) => {
-            return allGood && error_messages[name] === '';
-        }, true);
-    }
-
-    //check if the age, height and weight are numbers
-    //this is only for testing and will not be integrated until the goals is done
-    checkIfContinousFieldsAreNumbers(emptyOrNot){
-        let newErrorMessages = this.allnames.reduce((acc, name) => {
-            let goalsField = name === 'goals';
-            if(!emptyOrNot[name] &&
-                    !goalsField
-                )
-            {
-                //there is some data in this field
-                let isNum = this.checkIfFullNum(this.state.fields[name]);
-                acc[name] = (!isNum) ? `${name[0].toUpperCase()}${name.substring(1)} must be a number!` : '';
-            }
-            else if(!goalsField){
-                acc[name] = emptyOrNot[name]; //if empty keep the original error
-            }
-            return acc;
-        }, {});
-
-        return newErrorMessages;
-    }
-
-    //doesn't check for decimal places which might be required for height and/or weight 
-    checkIfFullNum(field, fieldName){
-        return !field
-            .split('')
-            .find((char) => !'1234567890'.includes(char));
-    }
+    
 
     handleGoalOptionsChange(e){
         this.setState({
@@ -160,14 +92,14 @@ export default class GetStarted extends Component{
                         Goal
                         <select value={this.state.optionVal} onChange={(e) => this.handleGoalOptionsChange(e)}>
                             <option 
-                                style={{display: this.displayGoalDefaultOrNot(this.state.goalOptionVal)}} 
+                                style={{display: startUtils.displayGoalDefaultOrNot(this.state.goalOptionVal)}} 
                                 value='instruction'>
                                     Choose your desired goal
                             </option>
                             {
                                 this.options.map((option, i) => (
                                     <option key={i} value={option}>
-                                        {this.kebabCaseToWords(option)}
+                                        {startUtils.kebabCaseToWords(option)}
                                     </option>
                                 ))
                             }
@@ -179,19 +111,5 @@ export default class GetStarted extends Component{
                 </form>
             </div>
         );
-    }
-
-    displayGoalDefaultOrNot(optionVal){
-        //This makes sure that the user doesn't ignore the goals option
-        if(optionVal === 'instruction'){
-            return 'block';
-        }
-        return 'none';
-    }
-
-    kebabCaseToWords(words){
-        return words.split('-')
-            .map((word) => word[0].toUpperCase() +word.substring(1))
-            .join(' ');
     }
 }
