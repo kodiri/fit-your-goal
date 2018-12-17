435903724ab426953260a6082dedd6ae34342464
Osman Hussein <osman_990@hotmail.com>
2018-12-17 09:36:12 +0000

    removed the logic directory

--- src/logic/getDuration.js
+++
@@ -1,8 +0,0 @@
-import { filterByAge } from './filterByAge';
-import { filterByActivityLevel } from './filterByActivityLevel';
-
-export function getDuration(age, activityLevel, durations) {
-    durations = filterByAge(durations, age);
-    durations = filterByActivityLevel(durations, activityLevel);
-    return durations;
-};
