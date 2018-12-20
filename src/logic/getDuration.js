8a2fb75896c61357d03bb982c8e882a9148337cc
Tom Hodson <tomhodson91@hotmail.com>
2018-12-20 15:41:36 +0000

    Display personalised results on results page #21

--- src/logic/getDuration.js
+++ src/logic/getDuration.js
@@ -1,7 +1,8 @@
 import { filterByAge } from './filterByAge';
 import { filterByActivityLevel } from './filterByActivityLevel';
 
-export function getDuration(age, activityLevel, durations) {
+export function getDuration(age, activityLevel) {
+    let durations = [10, 20, 30, 40, 50, 60];
     durations = filterByAge(durations, age);
     durations = filterByActivityLevel(durations, activityLevel);
     return durations;
