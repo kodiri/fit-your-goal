a6c28d91d4996b378f20293fd052b67e96f40832
Tom Hodson <tomhodson91@hotmail.com>
2018-12-21 13:05:20 +0000

    Added icons on home page and shuffled

--- src/App.js
+++ src/App.js
@@ -4,16 +4,30 @@ import home from './home'
 import getStarted from './getStarted'
 import results from './results'
 import './App.css';
+import { Link } from 'react-router-dom';
+import homeIcon from './iconmonstr-home-6 (1).svg'
+import fitIcon from './iconmonstr-medical-6.svg'
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className='App'>
-            <Route exact path='' component={home} />
-            <Route exact path='/get-started' component={getStarted} />
-            <Route exact path='/results' component={results} />
-        </div>
+          <div className='App'>
+            <nav className='banner'>
+            <div className='icon'>
+                  <Link to={'/'}> 
+                    <img className='imagee' src={homeIcon} alt=''/> 
+                  </Link>
+                </div>
+              <div className='icon'>
+                  <img className='imagee2' src={fitIcon} alt=''/> 
+              </div>
+                
+            </nav>
+              <Route exact path='' component={home} />
+              <Route exact path='/get-started' component={getStarted} />
+              <Route exact path='/results' component={results} />
+          </div>
       </Router>
     );
   }
