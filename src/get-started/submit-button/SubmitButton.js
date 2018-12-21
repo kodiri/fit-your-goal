2b9d16322e156d36bf00edaf54408d1068ead742
Osman Hussein <osman_990@hotmail.com>
2018-12-21 13:43:30 +0000

    match button

--- src/get-started/submit-button/SubmitButton.js
+++ src/get-started/submit-button/SubmitButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './SubmitButton.css';
 
 let submitButton = (props) => (
-    <input className='submit-button' type='submit' value='Get Started'/>
+    <input className='submit-button' type='submit' value='Fit My Goal!'/>
 );
 
 export default submitButton;
--- src/results.js
+++ src/results.js
@@ -4,15 +4,11 @@ import { getSports } from './logic/getSports'
 import { getDuration } from './logic/getDuration'
 import { getFrequency } from './logic/getFrequency'
 import { getIntensity } from './logic/getIntensity'
+import './results.css';
 
 export default function results(props) {
     return (
-        <div>
-            <Link to= {'/'}>
-                <div className='links'>
-                    Home
-                </div>
-            </Link>
+        <div className='results-info'>
             <h1>Your personal fitness program!</h1>
             <h2>Try these activities: {getSports(Number(props.history.location.state.age), Number(props.history.location.state.weight),(props.history.location.state.goals)).join(', ')}</h2>
             <h2>Train between {getDuration(Number(props.history.location.state.age), Number(props.history.location.state.activity))[0]} and {getDuration(Number(props.history.location.state.age), Number(props.history.location.state.activity)).slice(-1)} minutes!</h2>
